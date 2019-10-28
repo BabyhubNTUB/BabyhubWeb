63504aea61b2b3c9cfe8ee4703b9a5ab0e8a5b52
vivian0124 <vi881218@yahoo.com>
2019-10-28 21:33:34 +0800

    update babyhub
    Signed-off-by: vivian0124 <vi881218@yahoo.com>

--- routes/like.js
+++ routes/like.js
@@ -17,12 +17,28 @@ router.post('/', function (req, res, next) {
     }
     console.log(newData);
     like.like(newData).then(d => {
-        console.log('d');
-        console.log(d);
         if (d==0) {
-            res.render('userSuccess');  //傳至成功頁面
+            var id = req.session.userid;
+            noti.list(id).then(noti => {
+                if (noti == null) {
+                    res.render('error');  //導向錯誤頁面
+                } else if (noti == -1) {
+                    res.render('notFound');  //導向找不到頁面                
+                } else {              
+                    res.render('userSuccess', {noti:noti});  //將資料傳給顯示頁面
+                }
+            })
         } else {
-            res.render('userFail');     //導向錯誤頁面
+            var id = req.session.userid;
+            noti.list(id).then(noti => {
+                if (noti == null) {
+                    res.render('error');  //導向錯誤頁面
+                } else if (noti == -1) {
+                    res.render('notFound');  //導向找不到頁面                
+                } else {              
+                    res.render('userFail', {noti:noti});  //將資料傳給顯示頁面
+                }
+            })
         }
     })
 });
