2a17ee08c00ff66f30856a75d7854d85edf8e779
vivian0124 <vi881218@yahoo.com>
2019-11-15 20:17:17 +0800

    update babyhubweb
    Signed-off-by: vivian0124 <vi881218@yahoo.com>

--- routes/search.js
+++ routes/search.js
@@ -4,6 +4,7 @@ var router = express.Router();
 //增加引用函式
 const articlelist = require('./utility/forum');
 const noti = require('./utility/notification');
+var moment = require('moment');
 
 //接收GET請求
 router.get('/', function (req, res, next) {
@@ -22,12 +23,21 @@ router.get('/', function (req, res, next) {
                     } else if (noti == -1) {
                         res.render('notFound', { noti: noti });  //導向找不到頁面                
                     } else {
+                        for (var i = 0; i < data.forum.length; i++) {
+                            data.forum[i].forumdate = moment(data.forum[i].forumdate).format("YYYY-MM-DD");
+                        }
                         res.render('forum', { result: data, noti: noti });  //將資料傳給顯示頁面
                     }
                 })
             } else if (check == 2) {
+                for (var i = 0; i < data.forum.length; i++) {
+                    data.forum[i].forumdate = moment(data.forum[i].forumdate).format("YYYY-MM-DD");
+                }
                 res.render('forum2', { result: data });  //將資料傳給顯示頁面
             } else {
+                for (var i = 0; i < data.forum.length; i++) {
+                    data.forum[i].forumdate = moment(data.forum[i].forumdate).format("YYYY-MM-DD");
+                }
                 res.render('forum3', { result: data });  //將資料傳給顯示頁面
             }
         } else {
