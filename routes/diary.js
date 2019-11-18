5e7637abcb81e911fe23da38988bf881d1227c7a
vivian0124 <vi881218@yahoo.com>
2019-11-18 20:18:48 +0800

    update babyhubweb
    Signed-off-by: vivian0124 <vi881218@yahoo.com>

--- routes/diary.js
+++ routes/diary.js
@@ -11,25 +11,19 @@ router.get('/', function (req, res, next) {
   //取出參數
   var userid = req.session.userid;
   diary.list(userid).then(data => {
-    console.log(data);
-    console.log(data.diary.length);
     if (data == null) {
       res.render('error');  //導向錯誤頁面
     } else if (data.diary.length >= 0) {
-      console.log("-----------")
       for (var i = 0; i < data.diary.length; i++) {
         data.diary[i].diarydate = moment(data.diary[i].diarydate).format("YYYY-MM-DD");
       }
       var id = req.session.userid;
-      console.log(req.session.userid);
       noti.list(id).then(noti => {
-        console.log(noti);
         if (noti == null) {
           res.render('error');  //導向錯誤頁面
         } else if (noti == -1) {
           res.render('notFound', { noti: noti });  //導向找不到頁面                
         } else {
-          console.log(data);
           res.render('diary', { item: data, noti: noti });  //將資料傳給顯示頁面
         }
       })
